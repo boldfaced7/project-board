d8f390cdb3aadd08fd6518e7cbe262b4016d1168
boldfaced7 <boldfaced753@gmail.com>
2023-10-20 14:47:14 +0900

    #23 - 서비스 로직 테스트 코드 업데이트 및 DTO 재설계, 쿼리 메소드 작성
    나머지 로직의 테스트 코드를 작성하고 연관된 DTO 를 재설계
    테스트가 필요로 하는 repository 쿼리 메소드도 작성

--- src/main/java/com/project/board/service/ArticleService.java
+++ src/main/java/com/project/board/service/ArticleService.java
@@ -2,10 +2,11 @@ package com.project.board.service;
 
 import com.project.board.domain.type.SearchType;
 import com.project.board.dto.ArticleDto;
-import com.project.board.dto.ArticleUpdateDto;
+import com.project.board.dto.ArticleWithCommentsDto;
 import com.project.board.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,12 +18,12 @@ public class ArticleService {
     private final ArticleRepository articleRepository;
 
     @Transactional(readOnly = true)
-    public Page<ArticleDto> searchArticles(SearchType type, String searchKeyword) {
+    public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
         return Page.empty();
     }
 
     @Transactional(readOnly = true)
-    public ArticleDto searchArticle(long articleId) {
+    public ArticleWithCommentsDto getArticle(long articleId) {
         return null;
     }
 
@@ -30,7 +31,7 @@ public class ArticleService {
 
     }
 
-    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+    public void updateArticle(long articleId, ArticleDto dto) {
 
     }
 
