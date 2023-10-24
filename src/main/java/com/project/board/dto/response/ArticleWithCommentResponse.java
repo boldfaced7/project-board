a2226daaa0aae0749c3c665d264affecf5cec23d
boldfaced7 <boldfaced753@gmail.com>
2023-10-25 01:14:42 +0900

    #47 - 수정, 삭제를 할 때 해당 사용자가 맞는지 확인하기 위한 응답 정보 추가
    인증 정보의 `userId`와 해당 게시글/댓글의 작성자 `userId`가 동일한지 알려면 응답에서 `userId`를 내려줘야
    이를 response dto 에 반영

--- src/main/java/com/project/board/dto/response/ArticleWithCommentResponse.java
+++ src/main/java/com/project/board/dto/response/ArticleWithCommentResponse.java
@@ -15,11 +15,12 @@ public record ArticleWithCommentResponse(
         LocalDateTime createdAt,
         String email,
         String nickname,
+        String userId,
         Set<ArticleCommentsResponse> articleCommentsRespons
 ) {
 
-    public static ArticleWithCommentResponse of(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String email, String nickname, Set<ArticleCommentsResponse> articleCommentsRespons) {
-        return new ArticleWithCommentResponse(id, title, content, hashtag, createdAt, email, nickname, articleCommentsRespons);
+    public static ArticleWithCommentResponse of(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String email, String nickname, String userId, Set<ArticleCommentsResponse> articleCommentsResponse) {
+        return new ArticleWithCommentResponse(id, title, content, hashtag, createdAt, email, nickname, userId, articleCommentsResponse);
     }
 
     public static ArticleWithCommentResponse from(ArticleWithCommentsDto dto) {
@@ -36,6 +37,7 @@ public record ArticleWithCommentResponse(
                 dto.createdAt(),
                 dto.userAccountDto().email(),
                 nickname,
+                dto.userAccountDto().userId(),
                 dto.articleCommentDtos().stream()
                         .map(ArticleCommentsResponse::from)
                         .collect(Collectors.toCollection(LinkedHashSet::new))
